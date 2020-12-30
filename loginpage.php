40245a2298858da92b0d1d40fe2293b4054e38b8
Marcin Dubinski <m@MacBook-Pro-Marcin.local>
2020-12-30 21:32:49 +0100

    initial styles added

---
+++ loginpage.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>
+        Notes-Manager Pro
+    </title>
+    <link rel="stylesheet" href="css/regpage.css" />
+</head>
+
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="index.php">Main page</a></li>
+                <li><a href="loginpage.php">Log in</a></li>
+                <li><a href="regpage.php">Sing in now!</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <h1>Welcome in Notes Manager Pro</h1>
+    <h2>System to manage your notes!</h2>
+    <h2>Follow your dreams and write everything you want! </h2>
+  
+</body>
+
+</html>
