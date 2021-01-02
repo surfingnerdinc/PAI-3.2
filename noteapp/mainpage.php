18b5144200b24843940503ca993005f825d5dd29
Marcin Dubinski <m@MacBook-Pro-Marcin.local>
2021-01-02 12:18:14 +0100

    positive result of login and switching to noteapp pages

---
+++ noteapp/mainpage.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>
+        Notes-Manager Pro
+    </title>
+    <link rel="stylesheet" href="css/loginpage.css" />
+</head>
+
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="mainpage.php">Main page</a></li>
+                <li><a href="notepage.php">Notes</a></li>
+                <li><a href="accountpage.php">Account!</a></li>
+            </ul>
+        </nav>
+    </header>
+<h1>Main page after login</h1>
+
+</body>
+
+</html>
--- phpscr/zaloguj.php
+++ phpscr/zaloguj.php
@@ -11,7 +11,36 @@
     $userNa = $_POST['userName'];
     $userPass = $_POST['userPassword'];
 
-    echo "it works";
+    $sqlquerry = "SELECT * FROM users WHERE userMail = '$userNa' AND userPassword = '$userPass'";
+
+    if ($res = @$connect -> query($sqlquerry)){
+        $tmpusers = $res -> num_rows;
+        if($tmpusers > 0){
+            $wiersz = $res -> fetch_assoc();
+            // $mail_tab = $wiersz['userMail'];
+            // $name_tab = $wiersz['firstName'];
+            // $lname_tab = $wiersz['lastName'];
+
+
+            /* czyszczenie RAMu - jedna funkcja. */
+            //$res -> free();
+            //$res -> close();
+            $res -> free_result();
+
+            /* 
+            mozemy odwolac sie do wierszy tabeli asocjacyjnej poprzez: 
+            in e.g. => echo "you are logged as ".$wiersz['lastName'].", ".$wiersz['firstName'];
+            */
+
+            //przenoszenie do kolejnej strony: 
+            header('Location: ../noteapp/mainpage.php');
+
+        } else {
+
+        }
+    }   
+
+
 
     $connect -> close();
  }
